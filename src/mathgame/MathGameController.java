0bcc5d6cf606cb8b2e452774c8ca5200c71c218c
kashifo <koderkashif@gmail.com>
2020-03-25 23:50:33 +0530

    Started working on math game.

---
+++ src/mathgame/MathGameController.java
@@ -0,0 +1,20 @@
+package mathgame;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class MathGameController {
+
+    public javafx.scene.control.Label labelOne;
+    public javafx.scene.control.Label labelTwo;
+    public Label labelTitle;
+    public TextField textFieldAnswer;
+    public Button btnSubmit;
+    int count = 0;
+
+    public void setBtnSubmit(ActionEvent actionEvent) {
+
+    }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,8 +9,15 @@ import javafx.event.ActionEvent;
 public class Controller {
 
     public javafx.scene.control.Label labelOne;
+    public javafx.scene.control.Label labelCount;
+    int count = 0;
 
     public void sayHello(ActionEvent actionEvent) {
-        labelOne.setText("Alhamdulillah!");
+        count += 1;
+
+        labelCount.setText( String.valueOf(count) );
+
+        if(count==3)
+            labelOne.setText("Alhamdulillah!");
     }
 }
